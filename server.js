2fab341be893bee66cd800ebab6cb071c115b9a2
Gautham Arumugam <gaut1993@gmail.com>
2022-03-09 14:45:24 +0100

    add more functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const dotenv = require('dotenv')
 const express = require('express')
+const server = http.createServer(app)
 const app = express()
 
 
@@ -8,4 +9,9 @@ app.use(express.static('frontend-new/build'))
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+const port = process.env.PORT || 8000
+server.listen(port, () => {
+    console.log(`Server started at port: ${port}`)
 })
